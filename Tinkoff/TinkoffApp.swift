d420c266641f84b635238d9af048013326cfa8aa
amazingtima <timur.tansikbaev@gmail.com>
2023-02-05 16:02:03 +0600

    Initial Commit

---
+++ Tinkoff/TinkoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinkoffApp.swift
+//  Tinkoff
+//
+//  Created by Тимур on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TinkoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
